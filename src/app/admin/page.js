7f8e48dc7c0cd0529f47c4193a422875f4565d9b
Hein <u6632106@au.edu>
2026-03-06 11:31:30 +0700

    changes to admin/page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -5,6 +5,8 @@ export default function Admin(){
 
 const [title,setTitle]=useState("")
 const [author,setAuthor]=useState("")
+const [quantity,setQuantity]=useState("")
+const [location,setLocation]=useState("")
 const [message,setMessage]=useState("")
 
 const addBook = async ()=>{
@@ -16,12 +18,13 @@ headers:{
 },
 body:JSON.stringify({
 title,
-author
+author,
+quantity:Number(quantity),
+location
 })
 })
 
 const data = await res.json()
-
 setMessage(data.message)
 
 }
@@ -33,7 +36,7 @@ return(
 <h1>Admin Panel</h1>
 
 <input
-placeholder="Book title"
+placeholder="Title"
 value={title}
 onChange={(e)=>setTitle(e.target.value)}
 />
@@ -48,6 +51,22 @@ onChange={(e)=>setAuthor(e.target.value)}
 
 <br/>
 
+<input
+placeholder="Quantity"
+value={quantity}
+onChange={(e)=>setQuantity(e.target.value)}
+/>
+
+<br/>
+
+<input
+placeholder="Location"
+value={location}
+onChange={(e)=>setLocation(e.target.value)}
+/>
+
+<br/>
+
 <button onClick={addBook}>
 Add Book
 </button>
@@ -57,5 +76,4 @@ Add Book
 </div>
 
 )
-
 }
